430c5e517c066eee6de67fae2fb38421a3d77226
Tore Sinding Bekkedal <tore.sinding.bekkedal@nav.no>
2021-10-17 15:31:01 +0200

    Improve the 404 page somewhat

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,28 @@
-export const Custom404 = () => {
-  return <h1>404 - Page Not Found</h1>;
-};
+import Head from "next/head";
+import React from "react";
+
+export const Custom404 = () => (
+   <>
+      <Head>
+         <title>nais blog - 404</title>
+      </Head>
+      <main>
+         <article>
+            <header>
+               <h1>404</h1>
+               <h2>We're sorry,</h2>
+               <p>
+                  the URL you have reached has been disconnected or it is no
+                  longer in service.
+               </p>
+               <p>
+                  If you feel you have reached this page in error, please check
+                  the URL and try your GET again.
+               </p>
+            </header>
+         </article>
+      </main>
+   </>
+);
 
 export default Custom404;
